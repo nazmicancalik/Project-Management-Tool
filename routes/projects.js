81fb792edcc3ec4eab3511135bcaed6ca6381b8d
nazmicancalik <nazmicancalik@gmail.com>
2018-05-06 22:17:52 +0300

    Add new project Style

--- routes/projects.js
+++ routes/projects.js
@@ -13,4 +13,35 @@ router.get("/", function(req, res, next) {
     });
 });
 
+// ADD A NEW PROJECT
+router.get("/new", (req, res) => {
+  res.render("newProject");
+});
+
+// GET A SPECIFIC PROJECT
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  respondAndRenderProject(id, res, "singleProject");
+});
+
+function respondAndRenderProject(id, res, viewName) {
+  if (validId(id)) {
+    knex("projects")
+      .select()
+      .where("id", id)
+      .first()
+      .then(project => {
+        res.render(viewName, project);
+      });
+  } else {
+    res.status(500);
+    res.render("error", {
+      message: "Invalid id"
+    });
+  }
+}
+
+function validId(id) {
+  return !isNaN(id);
+}
 module.exports = router;
