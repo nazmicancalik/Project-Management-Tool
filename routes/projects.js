301e4922598f365eab7aa310dda6b621a1cf96d5
nazmicancalik <nazmicancalik@gmail.com>
2018-05-07 01:02:31 +0300

    Add task feature is added.

--- routes/projects.js
+++ routes/projects.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const knex = require("../db/knex");
 
 /* This Router Starts from /projects */
+/* ************** GET ROUTES ************** */
 router.get("/", function(req, res, next) {
   knex("projects")
     .select()
@@ -16,6 +17,12 @@ router.get("/", function(req, res, next) {
 // ADD A NEW PROJECT
 router.get("/new", (req, res) => {
   res.render("newProject");
+});
+
+// ADD A NEW TASK
+router.get("/:id/tasks/new", (req, res) => {
+  const id = req.params.id;
+  res.render("newTask", { project_id: id });
 });
 
 // GET A SPECIFIC PROJECT
@@ -37,7 +44,7 @@ router.get("/:project_id/tasks/:task_id", (req, res) => {
   respondAndRenderSingleTask(task_id, res, "singleTask");
 });
 
-/* *************** POSTS ********************* */
+/* ************** POST ROUTES ************** */
 // CREATE A PROJECT
 router.post("/", (req, res) => {
   validateProjectRenderError(req, res, project => {
@@ -46,6 +53,19 @@ router.post("/", (req, res) => {
       .then(ids => {
         const id = ids[0];
         res.redirect(`/projects/${id}`);
+      });
+  });
+});
+
+router.post("/:project_id/tasks", (req, res) => {
+  const project_id = req.params.project_id;
+  validateTaskRenderError(req, res, task => {
+    task.project_id = project_id;
+    knex("tasks")
+      .insert(task, "id")
+      .then(ids => {
+        const id = ids[0];
+        res.redirect(`/projects/${project_id}/tasks/${id}`);
       });
   });
 });
@@ -74,7 +94,7 @@ function respondAndRenderTasks(id, res, viewName) {
       .where("project_id", id)
       .then(tasks => {
         console.log(JSON.stringify(tasks, undefined, 2));
-        res.render(viewName, { tasks: tasks });
+        res.render(viewName, { tasks: tasks, project_id: id });
       });
   } else {
     res.status(500);
@@ -119,6 +139,30 @@ function validateProjectRenderError(req, res, callback) {
       message: "Invalid todo"
     });
   }
+}
+
+function validateTaskRenderError(req, res, callback) {
+  console.log(req.body);
+  if (validTask(req.body)) {
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+      start_date: req.body.start_date,
+      duration: req.body.duration,
+      done: false
+    };
+
+    callback(task);
+  } else {
+    res.status(500);
+    res.render("error", {
+      message: "Invalid task"
+    });
+  }
+}
+
+function validTask(task) {
+  return typeof task.title == "string" && task.title.trim() != "";
 }
 
 function validProject(project) {
