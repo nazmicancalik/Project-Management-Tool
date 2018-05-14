ca86dbda562d17e305510279fd4f29e06264ba7a
nazmicancalik <nazmicancalik@gmail.com>
2018-05-14 11:22:04 +0300

    Two different local strategies are used for 2 types of login. Admin vs Manager.

--- routes/index.js
+++ routes/index.js
@@ -10,20 +10,38 @@ router.get("/", adminAuthenticationMiddleware(), function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+// Admin Login Page
 router.get("/adminLogin", function(req, res, next) {
   res.render("adminLogin");
+});
+
+// Manager Login Page
+router.get("/managerLogin", function(req, res, next) {
+  res.render("managerLogin");
 });
 
 /* ************ POST ROUTES ************ */
 // Admin Login
 router.post(
   "/adminLogin",
-  passport.authenticate("local", {
+  passport.authenticate("local.one", {
     successRedirect: "/",
     failureRedirect: "/adminLogin"
   }),
   (req, res) => {
     res.redirect("/");
+  }
+);
+
+// Manager Login
+router.post(
+  "/managerLogin",
+  passport.authenticate("local.two", {
+    //successRedirect: `/managers/${req.session.passport.userId}`,
+    failureRedirect: "/managerLogin"
+  }),
+  (req, res) => {
+    res.redirect(`/managers/${req.session.passport.user.id}`);
   }
 );
 
@@ -34,7 +52,9 @@ router.get("/logout", (req, res) => {
 });
 
 passport.use(
+  "local.one",
   new LocalStrategy((username, password, done) => {
+    // Admin Lookup
     knex("admins")
       .select()
       .where("email", username)
@@ -46,31 +66,70 @@ passport.use(
         if (admin.password !== password) {
           return done(null, false, { message: "Incorrect Password" });
         }
-        return done(null, { id: admin.id, isAdmin: true });
+        done(null, { id: admin.id, isAdmin: true });
+        console.log("Looking up for an admin");
+        console.log(JSON.stringify(admin, undefined, 2));
       });
   })
 );
 
-passport.serializeUser((admin, done) => {
+passport.use(
+  "local.two",
+  new LocalStrategy((username, password, done) => {
+    // Manager Lookup
+    knex("managers")
+      .select()
+      .where("email", username)
+      .first()
+      .then(manager => {
+        if (!manager) {
+          return done(null, false, { message: "Incorrect Username" });
+        }
+        if (manager.password !== password) {
+          return done(null, false, { message: "Incorrect Password" });
+        }
+        console.log("Looking up for a manager");
+        console.log(JSON.stringify(manager, undefined, 2));
+        done(null, { id: manager.id, isAdmin: false });
+      });
+  })
+);
+
+passport.serializeUser((user, done) => {
   console.log("Serialize girdik");
-  done(null, admin);
+  done(null, user);
 });
 
-passport.deserializeUser((admin, done) => {
+passport.deserializeUser((user, done) => {
   console.log("Deserialize girdik");
-  knex("admins")
-    .select()
-    .where("id", admin.id)
-    .first()
-    .then(admin => {
-      done(null, admin);
-    });
+  if (user.isAdmin === true) {
+    knex("admins")
+      .select()
+      .where("id", user.id)
+      .first()
+      .then(user => {
+        if (user) {
+          return done(null, user);
+        }
+      });
+  } else {
+    console.log(JSON.stringify(user, undefined, 2));
+    knex("managers")
+      .select()
+      .where("id", user.id)
+      .first()
+      .then(res => {
+        if (res) {
+          return done(null, res);
+        }
+      });
+  }
 });
 
 function adminAuthenticationMiddleware() {
   return (req, res, next) => {
     console.log(
-      `req.session.passport.user: ${JSON.stringify(
+      `rtrtrtrtreq.session.passport.user: ${JSON.stringify(
         req.session.passport,
         undefined,
         2
